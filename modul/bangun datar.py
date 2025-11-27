1af1a1582e3e246b0dabdc237fb6c4f38afe8977
audyacantika-lab <audyacantika1906@gmail.com>
2025-11-27 11:17:00 +0700

    Add files via upload

---
+++ modul/bangun datar.py
@@ -0,0 +1,6 @@
+print(" BANGUN DATAR ~~")
+print(f"Luas Persegi adalah ")
+print(f"Luas Persegi Panjang adalah ")
+print(f"Luas Segitiga adalah ")
+print(f"Luas Lingkaran adalah ")
+print(f"Luas JajarGanjar adalah ")
